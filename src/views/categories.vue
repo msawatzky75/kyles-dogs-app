805a357f0731451a2ff1324b57e8859ddee40d8b
Spiffpitt <34319870+spiffpitt@users.noreply.github.com>
2019-03-29 12:42:08 -0500

    Added categories.

---
+++ src/views/categories.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<p class="title is-1">
+			Categories
+		</p>
+		<div class="columns is-multiline">
+			<div v-for="(category, key) in categories" :key="key" class="column is-3 is-6-touch">
+				<p class="title is-3">
+					{{ category.name }}
+				</p>
+				<p>{{ category.description }}</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Categories",
+	data() {
+		return {
+			categories: null
+		};
+	},
+	async created() {
+		this.categories = await (await fetch(`${this.$store.state.api}/product_categories`)).json();
+	}
+};
+</script>
