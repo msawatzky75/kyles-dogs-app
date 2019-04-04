cd3524057df91ff07fcdb5c2c0fc7d0c1ffd1572
Spiffpitt <34319870+spiffpitt@users.noreply.github.com>
2019-04-04 09:09:36 -0500

    Added basic pagination

---
+++ src/components/pagination.vue
@@ -0,0 +1,39 @@
+<template>
+	<nav v-if="range" class="pagination is-centered" role="navigation" aria-label="pagination">
+		<router-link v-if="currentPage - 1 > 0" :to="{ name: $route.name, query: { ...$route.query, page: currentPage - 1 } }" class="pagination-previous" @click="$emit('changedPage', currentPage - 1)">
+			Previous
+		</router-link>
+		<router-link v-if="(currentPage - -1) < totalPages" :to="{ name: $route.name, query: {...$route.query, page: currentPage - -1 } }" class="pagination-next" @click="$emit('changedPage', currentPage - -1)">
+			Next
+		</router-link>
+
+		<ul class="pagination-list">
+			<li v-for="i in range" :key="i">
+				<router-link :to="{ name: $route.name, query: { ...$route.query, page: (start - 1 + i) } }" class="pagination-link" :class="{ 'is-active': currentPage == (start - 1 + i) }" @click="$emit('changedPage', (start - 1 + i))">
+					{{ start - 1 + i }}
+				</router-link>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+export default {
+	props: {
+		totalPages: {
+			type: Number,
+			required: true,
+			validator: val => val && Math.abs(val) == val
+		},
+		pageRange: {
+			type: Number,
+			default: 5
+		}
+	},
+	computed: {
+		currentPage() { return this.$route.query.page || 1; },
+		start() { return this.currentPage - Math.floor(this.range / 2) > 1 ? this.currentPage + Math.floor(this.range / 2) < this.totalPages ? this.currentPage - Math.floor(this.range / 2) : this.totalPages - (this.range - 1) : 1; },
+		range() { return this.pageRange < this.totalPages ? this.pageRange : this.totalPages; }
+	}
+};
+</script>
--- src/views/products.vue
+++ src/views/products.vue
@@ -13,21 +13,30 @@
 				<product :id="product" />
 			</div>
 		</div>
+
+		<div class="columns is-marginless">
+			<div class="column">
+				<pagination :total-pages="10" />
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 import Product from "@/components/product";
+import Pagination from "@/components/pagination";
 export default {
 	name: "Products",
 	components: {
-		Product
+		Product, Pagination
 	},
 	computed: {
+		totalPages() { return (this.$store.getters.get_search_by_query(this.$route.query) || { meta: {} }).meta.totalPages || NaN; },
 		products() { return (this.$store.getters.get_search_by_query(this.$route.query) || { products: [] }).products; }
 	},
 	watch: {
-		$route() { this.$store.dispatch("search", this.$route.query); }
+		$route() { this.$store.dispatch("search", this.$route.query); },
+		// "$store.state.search"
 	},
 	mounted() { this.$store.dispatch("search", this.$route.query); }
 };
